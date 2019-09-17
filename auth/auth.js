816cebb7d1c6e2129c458c533955ca9af8671219
Krsmace <Khallid.martin1@gmail.com>
2019-09-16 21:12:24 -0400

    added auth and more

---
+++ auth/auth.js
@@ -0,0 +1,11 @@
+const loginButton = document.getElementById("login-button");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const message = document.getElementById("login-message");
+
+loginButton.onclick = function(event) {
+	//message.textContent = emailInput.value + " has logged in :)";
+    
+    
+    firebase.auth().signInWithemailAndPassword(emailInput.value,passwordInput.value)
+};
