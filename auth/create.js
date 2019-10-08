e1a65af2e86e29d01472b2c5908488bcd54f250e
Krsmace <Khallid.martin1@gmail.com>
2019-10-07 22:55:47 -0400

    added repsonsive

--- auth/create.js
+++ auth/create.js
@@ -1,17 +1,30 @@
 /* sign up */
-const signupButton = document.getElementById("sign-up");
+
 const emailInput = document.getElementById("email");
+const userInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 const message = document.getElementById("login-message");
-const submitSignUp = document.getElementById("submit-sign-up");
+const submitSignUpButton = document.getElementById("submit-sign-up");
 
-signupButton.onclick = function(event) {
+submitSignUpButton.onclick = function(event) {
 	const promise = firebase.auth().createUserWithEmailAndPassword(emailInput.value, passwordInput.value);
     
-	promise.catch(function(error) {
-		message.textContent = error.message;
+    promise.catch(function(error) {
+        message.textContent = error.message;
 	});
+    
+    promise.then(function(credential){
+        createUser(credential.user.uid);
+    });
 };
+
+function createUser(uid){
+    const db = firebase.database();
+    const ref = db.ref('users').child(uid);
+    const promise = ref.update({
+        displayName: userInput.value
+    });
+}
 
 
 
